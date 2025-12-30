0ba0c9491d2e1e57db6c479429429829e78df3f9
molarmanful <ben@benpa.ng>
2025-12-30 13:14:27 -0500

    fix(ci): oh my god

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
               packages = with pkgs; [
                 taplo
                 yamlfmt
+                actionlint
                 mold
                 cargo-edit
               ];
